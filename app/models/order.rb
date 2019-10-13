24f34a6bf3015f70b0397016d51d860f1d014037
Risa Fujii <risa@kray.jp>
2019-10-13 11:26:36 +0900

    Execute BroadCastJob after create/update/destroy

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,5 @@
 class Order < ApplicationRecord
+  after_create_commit { OrderBroadcastJob.perform_later self }
+  after_update_commit { OrderBroadcastJob.perform_later self }
+  after_destroy { OrderBroadcastJob.perform_later self }
 end
