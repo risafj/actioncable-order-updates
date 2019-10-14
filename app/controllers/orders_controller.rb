a68fb5e8831ee7cce327f528f7165ace97c29eab
Risa Fujii <risaf1231@gmail.com>
2019-10-14 20:57:14 +0900

    Input form for adding orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,4 +2,15 @@ class OrdersController < ApplicationController
   def index
     @orders = Order.all
   end
+
+  def create
+    @order = Order.new(create_params)
+    @order.save
+  end
+
+  private
+
+  def create_params
+    params.permit(:item)
+  end
 end
